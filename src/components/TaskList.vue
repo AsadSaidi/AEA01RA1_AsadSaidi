10d9e48896a14148a9086b8988e2e45ceac4dccd
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-11-06 17:27:00 +0100

    made TaskList component: -  displays a list of all the tasks -  make eliminar-tasca emit and toggle-completed

---
+++ src/components/TaskList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul v-if="tasques.length > 0">
+    <TaskItem
+      v-for="(tasca, index) in tasques"
+      :key="index"
+      :tasca="tasca"
+      :index="index"
+      @eliminar-tasca="$emit('eliminar-tasca', $event)"
+      @toggle-completada="$emit('toggle-completada', $event)"
+    />
+  </ul>
+  <div v-else class="empty-state">
+    <p>No hi ha tasques per mostrar</p>
+    <small>Comença afegint la teva primera tasca</small>
+  </div>
+</template>
+
+<script setup>
+import TaskItem from './TaskItem.vue'
+const props = defineProps({
+  tasques: Array
+})
+</script>
+
+<style scoped src="../styles.css"></style>
+
