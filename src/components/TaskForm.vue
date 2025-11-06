f0b4bc70d7c114c8ade29733f6b335325cfb7248
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-11-06 17:24:26 +0100

    made taskForm component: -  form to add new tasks

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nova-tasca">
+    <input
+      v-model="inputTasca"
+      placeholder="Escriu una nova tasca..."
+      @keyup.enter="onAfegir"
+    />
+    <button @click="onAfegir" class="btn-primary">Afegir</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['afegir-tasca'])
+const inputTasca = ref('')
+
+function onAfegir() {
+  if (inputTasca.value.trim() !== '') {
+    emit('afegir-tasca', inputTasca.value)
+    inputTasca.value = ''
+  }
+}
+</script>
+
+<style scoped src="../styles.css"></style>
