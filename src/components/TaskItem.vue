8d1796dcbc4eb1d2cdb91d37f1a48ee4960d5ee1
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-11-06 17:19:54 +0100

    add TaskItem component: -  add trash png - add props tasca and index - made delete and toggle in cehckbox emits

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li :class="{ completada: tasca.completada }">
+    <label class="tasca-label">
+      <input type="checkbox" :checked="tasca.completada" @change="onToggleCompletada" />
+      <span>{{ tasca.titol }}</span>
+    </label>
+    <div class="tasca-actions">
+      <small class="data">{{ tasca.data }}</small>
+      <button @click="onEliminar" class="btn-delete">
+        <img src="../../public/trash.png" alt="Eliminar" />
+        </button>
+    </div>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  tasca: Object,
+  index: Number
+})
+const emit = defineEmits(['eliminar-tasca', 'toggle-completada'])
+
+function onEliminar() {
+  emit('eliminar-tasca', props.index)
+}
+function onToggleCompletada() {
+  emit('toggle-completada', props.index)
+}
+</script>
+
+<style scoped src="../styles.css"></style>
+
